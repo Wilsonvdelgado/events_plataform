f515667f4ca9aabada2f57841340cc7782460f22
Wilson Octavino Varela Delgado <wilsonvdelgado@gmail.com>
2022-07-02 23:07:10 -0100

    page video display

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./lib/apollo";
+import { BrowserRouter } from "react-router-dom";
 import { Event } from "./pages/Event";
+import { Router } from "./Router";
 
 function App() {
   return (
     <>
-      <Event />
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          {/* <Event /> */}
+          <Router />
+        </BrowserRouter>
+      </ApolloProvider>
     </>
   );
 }
--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,13 +1,16 @@
 import { Header } from "../Components/Header";
 import { Sideber } from "../Components/Sidebar";
 import { Video } from "../Components/Video";
+import { useParams } from "react-router-dom";
 
 export function Event() {
+  const { slug } = useParams<{ slug: string }>();
   return (
     <div className="flex flex-col">
       <Header />
       <main className="flex flex-1 min-h-screen">
-        <Video />
+        {slug ? <Video /> : <div className="flex-1"></div>}
+
         <Sideber />
       </main>
     </div>
