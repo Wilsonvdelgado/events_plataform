632b3c22608a44a0ec2458b5a22b7825f18263bf
Wilson Octavino Varela Delgado <wilsonvdelgado@gmail.com>
2022-07-06 23:09:47 -0100

    dados

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,22 +1,16 @@
-import { gql, useMutation } from "@apollo/client";
 import { useState, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
-
-const CREATE_SUBSCRIBE_MUTATION = gql`
-  mutation CreateSubscriber($name: String!, $email: String!) {
-    createSubscriber(data: { name: $name, email: $email }) {
-      id
-    }
-  }
-`;
+import { useCreateSubscriberMutation } from "../graphql/generated";
 
 export function Subscribe() {
   const navigate = useNavigate();
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
 
-  const [createSubscribe, { loading }] = useMutation(CREATE_SUBSCRIBE_MUTATION);
+  // const [createSubscribe, { loading }] = useMutation(CREATE_SUBSCRIBE_MUTATION);
+
+  const [createSubscribe, { loading }] = useCreateSubscriberMutation();
 
   async function handleSubscribe(event: FormEvent) {
     event.preventDefault();
